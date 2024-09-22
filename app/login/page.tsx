07a533ab9f533fab2f2666370d3255fa395b97db
Aniq-Sukairain <wananiqkk110@gmail.com>
2024-09-23 02:42:10 +0800

    user reporting submission + police data management
    add user reporting submission (HTML&CSS) & police user management (HTML&CSS)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -52,49 +52,49 @@ export default function LoginPage() {
 
     'use client';
     return (
-            <div className="login-container">
-                <div className="login-logo-text">
-                    <Image className="login-logo" src="/Images/loginlogo.png" alt="Logo" width={358} height={361}/>
-                    <div className="login-text">
-                        <div className="login-font-police">Police</div>
-                        <div className="login-font-reporting">Reporting</div>
-                        <div className="login-font-ai">Artificial Intelligence</div>
-                        <div className="login-font-system">System</div>
-                    </div>
-                </div>
-                <div className="login-form">
-                    <div className="login-form-container">
-                        <div className="login-form-bg"></div>
-                        <form onSubmit={Login}>
-                            <button className="login-login-button">
-                                Login
-                            </button>
-                            <div className="login-form-or">
-                                Or
-                            </div>
-                            <button className="login-register-button">
-                                Register
-                            </button>
-                            <div className="login-username-id">
-                                <input type="text" name="username" placeholder="Username or id..." className="login-username-id-textfield" required/>
-                            </div>
-                            <div className="login-password">
-                                <input type="password" name="password" placeholder="Password" className="login-password-textfield" required/>
-                            </div>
-                            <div className="login-leftformmustache"></div>
-                            <div className="login-rightformmustache"></div>
-                            <div className="login-discription">
-                                This is an AI powered Police Reporting system. It provide online convenience for the public to submit police complaint via the Internet. This facility is specially provided for lodging police report pertaining to case of lost document or item that does not relate to crime.
-                            </div>
-                            <div className="login-title">
-                                Welcome to
-                            </div>
-                            <div className="login-subtitle">
-                                Police Reporting Ai System
-                            </div>
-                        </form>
-                    </div>
+        <div className="login-container">
+            <div className="login-logo-text">
+                <Image className="login-logo" src="/Images/loginlogo.png" alt="Logo" width={358} height={361}/>
+                <div className="login-text">
+                    <div className="login-font-police">Police</div>
+                    <div className="login-font-reporting">Reporting</div>
+                    <div className="login-font-ai">Artificial Intelligence</div>
+                    <div className="login-font-system">System</div>
                 </div>
             </div>
+            <div className="login-form">
+                <div className="login-form-container">
+                    <div className="login-form-bg"></div>
+                    <form onSubmit={Login}>
+                        <button className="login-login-button">
+                            Login
+                        </button>
+                        <div className="login-form-or">
+                            Or
+                        </div>
+                        <button className="login-register-button">
+                            Register
+                        </button>
+                        <div className="login-username-id">
+                            <input type="text" name="username" placeholder="Username or id..." className="login-username-id-textfield" required/>
+                        </div>
+                        <div className="login-password">
+                            <input type="password" name="password" placeholder="Password" className="login-password-textfield" required/>
+                        </div>
+                        <div className="login-leftformmustache"></div>
+                        <div className="login-rightformmustache"></div>
+                        <div className="login-discription">
+                            This is an AI powered Police Reporting system. It provide online convenience for the public to submit police complaint via the Internet. This facility is specially provided for lodging police report pertaining to case of lost document or item that does not relate to crime.
+                        </div>
+                        <div className="login-title">
+                            Welcome to
+                        </div>
+                        <div className="login-subtitle">
+                            Police Reporting Ai System
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     );
 };
