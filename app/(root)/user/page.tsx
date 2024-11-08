787b9cbbaf17c2c5b0f67432e54c15bdeb967b91
Zal Hazmi Bin Musrizal <zalhazmi.musrizal@student.gmi.edu.my>
2024-11-08 21:47:37 +0800

    Fix User Report Content

--- app/(root)/user/page.tsx
+++ app/(root)/user/page.tsx
@@ -7,48 +7,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faKeyboard } from '@fortawesome/free-regular-svg-icons'
 import { faMicrophoneLines } from '@fortawesome/free-solid-svg-icons'
 import PHNavBar from '@/components/PHNavBar';
+import ReportContent from '@/components/ReportContent';
 import { ReportType } from '@/types/index.d';
 // import NavBar from '../../components/userNavBar';
 
+const UserMainPage = () => {
+  return (
+    <div>
+        <PHNavBar />
+        <ReportContent />
+    </div>
+  )
+}
 
-export default function UserReportingSubmission() {
-    
-    const router = useRouter();
-    const [reportTypes, setReportTypes] = useState<ReportType[]>([]);
-    const [selectedReportType, setSelectedReportType] = useState<string>('');
+export default UserMainPage
 
-    useEffect(() => {
-        async function fetchReportType() {
-            const response = await fetch('http://localhost:5035/api/reporttype', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-    
-            const data = await response.json();
-            setReportTypes(data);
-        }
-
-        fetchReportType();
-    }, []);
-
-    
-
-    function newReportButton(e: React.MouseEvent){
-        e.preventDefault();
-        if(selectedReportType === '0'){
-            alert('Please select a report type');
-            return;
-        }
-        router.push(`/new-report/${selectedReportType}`);
-    }
-
-    return (
-        <div className=" flex items-center justify-center mt-[16vh]"> {/* PAGE BODY */}
-                <PHNavBar/>
-                
-        </div>
-    );
-};
 
